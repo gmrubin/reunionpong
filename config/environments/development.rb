feee3caba4d57d1eb1e93aedf6584fe47eb4f89a
Gregg Rubin <gregg.m.rubin@gmail.com>
2013-02-18 15:56:19 -0500

    installing devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,24 @@ Reunionpong::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # A dummy setup for development - no deliveries, but logged
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  # ^^ ADDED FOR DEVISE ^^
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "reunionpong.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["EMAIL_USERNAME"],
+    password: ENV["EMAIL_PASSWORD"]
+  }
+
 end
