f3d15bfe30d2dbb675979212e930e968f29bfd40
Gregg Rubin <gregg.m.rubin@gmail.com>
2013-02-18 15:17:43 -0500

    second commit, this time with codebase

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reunionpong::Application.config.secret_token = 'c5ed5967380ff36dfb0a54c6f591661b89fa565dbe64d233f831655c556798af140acbb979c790a99cb1fcb5768bcdc89ac532f7d4fc3306ea320b2e6a3e0abe'
