e5486715cff4664c8152ed053e9994d6c945d25b
Gregg Rubin <gregg.m.rubin@gmail.com>
2013-02-24 16:11:00 -0500

    Adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
+puts ' *** Setting up default user login and roles ***'
+
+puts 'ROLES'
+YAML.load(ENV['ROLES']).each do |role|
+  Role.find_or_create_by_name("role")
+  puts 'role: ' << role
+end
+
+puts 'GENERATING WHITELIST'
+Whitelist.create!(email: ENV['ADMIN_EMAIL'].dup)
+
+puts 'DEFAULT USERS'
+user = User.create!(email: ENV['ADMIN_EMAIL'].dup,
+                    password: ENV['ADMIN_PASSWORD'].dup, password_confirmation: ENV['ADMIN_PASSWORD'].dup)
+user.confirm!
+puts 'user: ' << user.email
+user.add_role :admin
