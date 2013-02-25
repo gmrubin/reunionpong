c383a8d5a058d33d5c45978c6d306aaf51bacffc
Gregg Rubin <gregg.m.rubin@gmail.com>
2013-02-24 21:40:34 -0500

    Adds authentication on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_user!
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_path, :alert => exception.message
+  end
+
+  def after_sign_in_path_for(user)
+    teams_path
   end
 end
