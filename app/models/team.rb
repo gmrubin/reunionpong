63e38f29e98c26e15030340f1b344557eab42e3e
Gregg Rubin <gregg.m.rubin@gmail.com>
2013-03-04 20:57:37 -0500

    adds uniqueness validation to team

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,6 +3,7 @@ class Team < ActiveRecord::Base
   has_many :users
 
   validates :name, presence: true
+  validates_uniqueness_of :name, case_sensitive: false
   validates :city, presence: true
 
   before_validation :validate_pair
